a20c8ed62abbb35ff689a92aacd529beba1b97ff
Naral <77269201+NaralC@users.noreply.github.com>
2023-09-09 00:58:05 +0100

    db migration + write endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,27 @@
-import { publicProcedure, router } from "@/server/trpc-server"
+import Database from "better-sqlite3";
+import { publicProcedure, router } from "@/server/trpc-server";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { todos } from "@/db/schema";
+import { z } from "zod";
+
+const db = drizzle(new Database("sqlite.db"));
+migrate(db, { migrationsFolder: "drizzle" });
 
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
-    return [10, 20, 30000];
+    return db.select().from(todos).all();
+  }),
+  addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
+    await db
+      .insert(todos)
+      .values({
+        content: opts.input,
+        done: 0,
+      })
+      .run();
+
+    return true
   }),
 });
 
