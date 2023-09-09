c86506148a91b5be5b57cf179f903b6d30ccb202
Naral <77269201+NaralC@users.noreply.github.com>
2023-09-09 01:07:31 +0100

    create CRUD

--- src/components/to-do-list.tsx
+++ src/components/to-do-list.tsx
@@ -1,10 +1,60 @@
 "use client";
 import { trpc } from "@/lib/trpc-client";
+import { useState } from "react";
 
 export default function ToDoList() {
-  const { data, isLoading } = trpc.getTodos.useQuery();
+  const { data, isLoading, refetch } = trpc.getTodos.useQuery();
+  const { mutate: addToDo } = trpc.addTodo.useMutation({
+    onSettled: () => {
+      refetch();
+    },
+  });
+
+  const [content, setContent] = useState("");
 
   if (isLoading) return <div>loading...</div>;
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div>
+      <div className="text-black my-5 text-3xl">
+        {data?.map((todo) => (
+          <div key={todo.id} className="flex gap-3 items-center">
+            <input
+              id={`check-${todo.id}`}
+              type="checkbox"
+              checked={!!todo.done}
+              style={{ zoom: 1.5 }}
+              // onChange={async () => {
+              //   setDone.mutate({
+              //     id: todo.id,
+              //     done: todo.done ? 0 : 1,
+              //   });
+              // }}
+            />
+            <label htmlFor={`check-${todo.id}`}>{todo.content}</label>
+          </div>
+        ))}
+      </div>
+      <div className="flex gap-3 items-center">
+        <label htmlFor="content">Content</label>
+        <input
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="flex-grow text-black bg-white rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-4 py-2"
+        />
+        <button
+          onClick={async () => {
+            if (content.length) {
+              addToDo(content);
+              setContent("");
+            }
+          }}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Add Todo
+        </button>
+      </div>
+    </div>
+  );
 }
